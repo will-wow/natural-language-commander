4d3565d4a760806e63ff127589e391851b99f604
Will <will@whentheresawill.net>
2016-05-25 02:04:38 -0700

    move tests into big describe

--- src/test/nlc.ts
+++ src/test/nlc.ts
@@ -31,37 +31,7 @@ function expectCommandToNotMatch(nlc: NLC, command: string, matchCallback, noMat
     .catch((error) => done(error));
 }
 
-describe('basic commands', () => {
-  let nlc: NLC;
-  let matchCallback;
-  let noMatchCallback;
-
-  beforeEach(() => {
-    nlc = new NLC();
-
-    matchCallback = chai.spy();
-    noMatchCallback = chai.spy();
-
-    // Register a simple intent.
-    nlc.registerIntent({
-      intent: 'TEST',
-      callback: matchCallback,
-      utterances: [
-        'test'
-      ]
-    });
-  });
-
-  it('should match a simple input', (done) => {
-    expectCommandToMatch(nlc, 'test', matchCallback, noMatchCallback, done);
-  });
-
-  it('should not match a simple bad input', (done) => {
-     expectCommandToNotMatch(nlc, 'tset', matchCallback, noMatchCallback, done);
-  });
-});
-
-describe('slot types', () => {
+describe('NLC', () => {
   let nlc: NLC;
   let matchCallback;
   let noMatchCallback;
@@ -73,139 +43,165 @@ describe('slot types', () => {
     noMatchCallback = chai.spy();
   });
 
-  describe('STRING', () => {
+  describe('basic commands', () => {
     beforeEach(() => {
-      // Register an intent with a STRING.
+      // Register a simple intent.
       nlc.registerIntent({
-        intent: 'STRING_TEST',
+        intent: 'TEST',
         callback: matchCallback,
-        slots: [
-          {
-            name: 'String',
-            type: 'STRING'
-          }
-        ],
         utterances: [
-          'test {String} test'
+          'test'
         ]
       });
     });
 
-    it('should match a string slot', (done) => {
-      expectCommandToMatch(nlc, 'test this is a string test', matchCallback, noMatchCallback, done);
+    it('should match a simple input', (done) => {
+      expectCommandToMatch(nlc, 'test', matchCallback, noMatchCallback, done);
     });
 
-    it('should not match a bad string slot', (done) => {
-      expectCommandToNotMatch(nlc, 'test test', matchCallback, noMatchCallback, done);
+    it('should not match a simple bad input', (done) => {
+      expectCommandToNotMatch(nlc, 'tset', matchCallback, noMatchCallback, done);
     });
   });
 
-  describe('DATE', () => {
-    beforeEach(() => {
-      // Register an intent with a STRING.
-      nlc.registerIntent({
-        intent: 'DATE_TEST',
-        callback: matchCallback,
-        slots: [
-          {
-            name: 'Date',
-            type: 'DATE'
-          }
-        ],
-        utterances: [
-          'test {Date} test'
-        ]
+  describe('slot types', () => {
+    describe('STRING', () => {
+      beforeEach(() => {
+        // Register an intent with a STRING.
+        nlc.registerIntent({
+          intent: 'STRING_TEST',
+          callback: matchCallback,
+          slots: [
+            {
+              name: 'String',
+              type: 'STRING'
+            }
+          ],
+          utterances: [
+            'test {String} test'
+          ]
+        });
+      });
+
+      it('should match a string slot', (done) => {
+        expectCommandToMatch(nlc, 'test this is a string test', matchCallback, noMatchCallback, done);
+      });
+
+      it('should not match a bad string slot', (done) => {
+        expectCommandToNotMatch(nlc, 'test test', matchCallback, noMatchCallback, done);
       });
     });
-    
-    it('should match a date slot with a MM/DD/YYYY date', (done) => {
-      expectCommandToMatch(nlc, 'test 10/10/2016 test', matchCallback, noMatchCallback, done);
+
+    describe('DATE', () => {
+      beforeEach(() => {
+        // Register an intent with a STRING.
+        nlc.registerIntent({
+          intent: 'DATE_TEST',
+          callback: matchCallback,
+          slots: [
+            {
+              name: 'Date',
+              type: 'DATE'
+            }
+          ],
+          utterances: [
+            'test {Date} test'
+          ]
+        });
+      });
+
+      it('should match a date slot with a MM/DD/YYYY date', (done) => {
+        expectCommandToMatch(nlc, 'test 10/10/2016 test', matchCallback, noMatchCallback, done);
+      });
+
+      it('should match a date slot with a YYYY-MM-DD date', (done) => {
+        expectCommandToMatch(nlc, 'test 2016-10-10 test', matchCallback, noMatchCallback, done);
+      });
+
+      it('should match a date slot with a MMM DD, YYYY date', (done) => {
+        expectCommandToMatch(nlc, 'test Oct 10, 2016 test', matchCallback, noMatchCallback, done);
+      });
+
+      it('should match a date slot with a MMMM DD, YYYY date', (done) => {
+        expectCommandToMatch(nlc, 'test October 10, 2016 test', matchCallback, noMatchCallback, done);
+      });
+
+      it('should match a date slot with "today"', (done) => {
+        expectCommandToMatch(nlc, 'test today test', matchCallback, noMatchCallback, done);
+      });
+
+      it('should match a date slot with "tomorrow"', (done) => {
+        expectCommandToMatch(nlc, 'test tomorrow test', matchCallback, noMatchCallback, done);
+      });
+
+      it('should match a date slot with "yesterday"', (done) => {
+        expectCommandToMatch(nlc, 'test yesterday test', matchCallback, noMatchCallback, done);
+      });
+
+      it('should match a date slot with other strings', (done) => {
+        expectCommandToNotMatch(nlc, 'test foobar test', matchCallback, noMatchCallback, done);
+      });
     });
 
-    it('should match a date slot with a YYYY-MM-DD date', (done) => {
-      expectCommandToMatch(nlc, 'test 2016-10-10 test', matchCallback, noMatchCallback, done);
+    describe('SLACK_NAME', () => {
+      beforeEach(() => {
+        // Register an intent with a STRING.
+        nlc.registerIntent({
+          intent: 'SLACK_NAME_TEST',
+          callback: matchCallback,
+          slots: [
+            {
+              name: 'Name',
+              type: 'SLACK_NAME'
+            }
+          ],
+          utterances: [
+            'test {Name} test'
+          ]
+        });
+      });
+
+      it('should match a name slot', (done) => {
+        expectCommandToMatch(nlc, 'test @test test', matchCallback, noMatchCallback, done);
+      });
+
+      it('should not match a bad name slot', (done) => {
+        expectCommandToNotMatch(nlc, 'test test test', matchCallback, noMatchCallback, done);
+      });
     });
-    
-    it('should match a date slot with a MMM DD, YYYY date', (done) => {
-      expectCommandToMatch(nlc, 'test Oct 10, 2016 test', matchCallback, noMatchCallback, done);
-    });
-    
-    it('should match a date slot with a MMMM DD, YYYY date', (done) => {
-      expectCommandToMatch(nlc, 'test October 10, 2016 test', matchCallback, noMatchCallback, done);
-    });
-    
-    it('should match a date slot with "today"', (done) => {
-      expectCommandToMatch(nlc, 'test today test', matchCallback, noMatchCallback, done);
-    });
-    
-    it('should match a date slot with "tomorrow"', (done) => {
-      expectCommandToMatch(nlc, 'test tomorrow test', matchCallback, noMatchCallback, done);
-    });
-    
-    it('should match a date slot with "yesterday"', (done) => {
-      expectCommandToMatch(nlc, 'test yesterday test', matchCallback, noMatchCallback, done);
-    });
-    
-    it('should match a date slot with other strings', (done) => {
-      expectCommandToNotMatch(nlc, 'test foobar test', matchCallback, noMatchCallback, done);
+
+    describe('SLACK_ROOM', () => {
+      beforeEach(() => {
+        // Register an intent with a STRING.
+        nlc.registerIntent({
+          intent: 'SLACK_ROOM_TEST',
+          callback: matchCallback,
+          slots: [
+            {
+              name: 'Room',
+              type: 'SLACK_ROOM'
+            }
+          ],
+          utterances: [
+            'test {Room} test'
+          ]
+        });
+      });
+
+      it('should match a room slot', (done) => {
+        expectCommandToMatch(nlc, 'test #test test', matchCallback, noMatchCallback, done);
+      });
+
+      it('should match a name slot', (done) => {
+        expectCommandToMatch(nlc, 'test @test test', matchCallback, noMatchCallback, done);
+      });
+
+      it('should not match a bad room slot', (done) => {
+        expectCommandToNotMatch(nlc, 'test test test', matchCallback, noMatchCallback, done);
+      });
     });
   });
   
-  describe('SLACK_NAME', () => {
-    beforeEach(() => {
-      // Register an intent with a STRING.
-      nlc.registerIntent({
-        intent: 'SLACK_NAME_TEST',
-        callback: matchCallback,
-        slots: [
-          {
-            name: 'Name',
-            type: 'SLACK_NAME'
-          }
-        ],
-        utterances: [
-          'test {Name} test'
-        ]
-      });
-    });
-
-    it('should match a name slot', (done) => {
-      expectCommandToMatch(nlc, 'test @test test', matchCallback, noMatchCallback, done);
-    });
-
-    it('should not match a bad name slot', (done) => {
-      expectCommandToNotMatch(nlc, 'test test test', matchCallback, noMatchCallback, done);
-    });
-  });
   
-  describe('SLACK_ROOM', () => {
-    beforeEach(() => {
-      // Register an intent with a STRING.
-      nlc.registerIntent({
-        intent: 'SLACK_ROOM_TEST',
-        callback: matchCallback,
-        slots: [
-          {
-            name: 'Room',
-            type: 'SLACK_ROOM'
-          }
-        ],
-        utterances: [
-          'test {Room} test'
-        ]
-      });
-    });
-
-    it('should match a room slot', (done) => {
-      expectCommandToMatch(nlc, 'test #test test', matchCallback, noMatchCallback, done);
-    });
-
-    it('should match a name slot', (done) => {
-      expectCommandToMatch(nlc, 'test @test test', matchCallback, noMatchCallback, done);
-    });
-
-    it('should not match a bad room slot', (done) => {
-      expectCommandToNotMatch(nlc, 'test test test', matchCallback, noMatchCallback, done);
-    });
-  });
 });
+
